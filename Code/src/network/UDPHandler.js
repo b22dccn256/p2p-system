73312dc29e2202d2c885cce5746fbca2da740d6b
Nguyễn Duy Hà <ha8893536@gmail.com>
2026-05-22 15:32:18 +0700

    feat: Triển khai cấu trúc dự án ban đầu với các tài sản giao diện người dùng và logic giao tiếp P2P.

--- Code/src/network/UDPHandler.js
+++ Code/src/network/UDPHandler.js
@@ -1,51 +1,41 @@
-// src/network/UDPHandler.js (Peer Discovery)
+// src/network/UDPHandler.js
 const dgram = require('dgram');
-const { BROADCAST_IP, DISCOVERY_INTERVAL } = require('../config/constants');
+const { UDP_PORT, BROADCAST_IP, DISCOVERY_INTERVAL } = require('../config/constants');
+const logger = require('../config/logger');
 
 class UDPHandler {
-    constructor(peer, port) {
+    constructor(peer) {
         this.peer = peer;
         this.socket = dgram.createSocket('udp4');
-        this.port = port;
     }
 
-    async startBroadcast() {
-        // 1. Bind socket để nhận broadcast từ peer khác
+    start() {
+        // Kích hoạt chế độ Broadcast của UDP
+        this.socket.bind(UDP_PORT, () => {
+            this.socket.setBroadcast(true);
+            logger.info(`📡 UDP Broadcast listening on port ${UDP_PORT}`);
+        });
+
+        // Lắng nghe tiếng "hét" từ các node khác trong LAN (Target 3)
         this.socket.on('message', (msg, rinfo) => {
-            const data = JSON.parse(msg.toString());
-            if (data.type === 'DISCOVERY_ANNOUNCE') {
-                this.peer.onPeerDiscovered(data.peerInfo);
-            }
-        });
-        await new Promise(resolve => this.socket.bind(this.port, resolve));
-
-        // 2. Định kỳ broadcast thông tin của chính mình
-        setInterval(() => this.broadcastSelf(), DISCOVERY_INTERVAL);
-    }
-
-    broadcastSelf() {
-        const message = JSON.stringify({
-            type: 'DISCOVERY_ANNOUNCE',
-            peerInfo: {
-                id: this.peer.id,
-                ip: this.getLocalIP(),
-                port: this.peer.tcpHandler.port
-            }
-        });
-        this.socket.send(message, 0, message.length, this.port, BROADCAST_IP);
-    }
-
-    getLocalIP() {
-        // Lấy IP local (bỏ qua 127.0.0.1)
-        const interfaces = require('os').networkInterfaces();
-        for (const iface of Object.values(interfaces)) {
-            for (const config of iface) {
-                if (config.family === 'IPv4' && !config.internal) {
-                    return config.address;
+            try {
+                const data = JSON.parse(msg.toString());
+                if (data.type === 'DISCOVERY' && data.peerId !== this.peer.id) {
+                    // Báo cho Peer Core biết vừa tìm thấy ai đó
+                    this.peer.onPeerDiscovered(data.peerId, rinfo.address, data.tcpPort, 'LAN');
                 }
-            }
-        }
-        return '127.0.0.1';
+            } catch (e) { }
+        });
+
+        // Định kỳ "hét" lên mạng LAN sự tồn tại của mình
+        setInterval(() => {
+            const message = JSON.stringify({
+                type: 'DISCOVERY',
+                peerId: this.peer.id,
+                tcpPort: this.peer.tcpPort
+            });
+            this.socket.send(message, 0, message.length, UDP_PORT, BROADCAST_IP);
+        }, DISCOVERY_INTERVAL);
     }
 }
 
