56c9e9398c01c820c130ba99d0dd7aec13fd6c3d
Nguyễn Duy Hà <ha8893536@gmail.com>
2026-05-27 19:52:57 +0700

    remove file

--- Code/churn-test.js
+++ Code/churn-test.js
@@ -4,11 +4,11 @@ const logger = require('./src/config/logger');
 // Mô phỏng Churn: Các Node tham gia và rời mạng liên tục
 async function runChurnSimulation() {
     logger.info("🌪️ Bắt đầu mô phỏng CHURN (Tắt/Mở Peer liên tục)...");
-    
-    const peers = [];
-    const NUM_PEERS = 5;
 
-    // Khởi tạo 5 peer
+    const peers = [];
+    const NUM_PEERS = 22;
+
+    // Khởi tạo 22 peer
     for (let i = 0; i < NUM_PEERS; i++) {
         const id = `churn_node_${i}`;
         const p = new Peer(id);
@@ -25,7 +25,7 @@ async function runChurnSimulation() {
         if (target.isOnline) {
             // Đang online -> Giả lập sập nguồn (Rời mạng)
             logger.warn(`[Churn Event] 🔴 Node ${target.id} ĐỘT NGỘT SẬP NGUỒN!`);
-            
+
             target.instance.isShuttingDown = true;
             // Gửi gói LEAVE
             const leaveMsg = JSON.stringify({ type: 'LEAVE', from: target.id }) + '\n';
@@ -38,17 +38,17 @@ async function runChurnSimulation() {
             target.instance.udpHandler.stop();
             target.instance.bootstrapClient.stop();
             target.isOnline = false;
-            
+
         } else {
             // Đang offline -> Khởi động lại (Tham gia mạng)
             logger.success(`[Churn Event] 🟢 Node ${target.id} ĐÃ KHÔI PHỤC KẾT NỐI!`);
-            
+
             // Tạo instance mới để thay thế
             target.instance = new Peer(target.id);
             await target.instance.start();
             target.isOnline = true;
         }
-        
+
     }, 4000); // Mỗi 4 giây có 1 node sập hoặc sống lại
 }
 
